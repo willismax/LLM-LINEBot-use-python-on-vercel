a0f99ea72d5c1de10dfd342c046dd813abd8f4ad
willismax <willismax.com@gmail.com>
2023-09-17 23:20:02 +0800

    project init

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
