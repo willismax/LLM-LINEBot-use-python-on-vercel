0c0e86d9d4835a1b5136b8b148f6a02998a7268c
willismax <willismax.com@gmail.com>
2023-10-10 02:50:26 +0800

    Fix llm.py

--- api/prompt.py
+++ api/prompt.py
@@ -8,20 +8,20 @@ LANGUAGE_TABLE = {
   "en": "Hello!"
 }
 
+AI_GUIDELINES = "你是一個AI助教，會代替老師初步回答問題，同學詢問課業的問題會用蘇格拉底式的教學方法引導學生思考解決方向，如果有需要會提醒學生跟老師確認""
+
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append({"role": "system", "content": f"{LANGUAGE_TABLE[chat_language]}, 你是一個AI助教，會代替老師初步回答問題，如果有需要會提醒學生跟老師確認"})
-            # f"AI:{LANGUAGE_TABLE[chat_language]}")
-    
+        self.msg_list.append(
+            {
+                "role": "system", 
+                "content": f"{LANGUAGE_TABLE[chat_language]}, {AI_GUIDELINES}"
+             })    
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
-            self.remove_msg()
+            self.msg_list.pop(0)
         self.msg_list.append({"role": "user", "content": new_msg})
 
-    def remove_msg(self):
-        self.msg_list.pop(0)
-
     def generate_prompt(self):
-        # return '\n'.join(self.msg_list)
         return self.msg_list
