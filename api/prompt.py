aa42871dd09b979236fac7debbf9d588e074ce5b
willismax <willismax.com@gmail.com>
2023-10-10 02:32:22 +0800

    Fix llm.py

--- api/prompt.py
+++ api/prompt.py
@@ -2,7 +2,7 @@ import os
 
 chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
 
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 7))
 LANGUAGE_TABLE = {
   "zh": "哈囉！",
   "en": "Hello!"
@@ -11,12 +11,13 @@ LANGUAGE_TABLE = {
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
+        self.msg_list.append({"role": "system", "content": "You are a helpful assistant."})
+            # f"AI:{LANGUAGE_TABLE[chat_language]}")
     
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
             self.remove_msg()
-        self.msg_list.append(new_msg)
+        self.msg_list.append({"role": "user", "content": new_msg})
 
     def remove_msg(self):
         self.msg_list.pop(0)
