01b3fccee1db7166b38529ecd825a4d2389fc8c2
willismax <willismax.com@gmail.com>
2023-10-10 01:23:50 +0800

    Fix handler to web_handler

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ from api.llm import ChatGPT
 import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+web_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
@@ -29,12 +29,12 @@ def callback():
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        web_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@web_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """LINE MessageAPI message processing"""
     if event.source.user_id == 'Udeadbeefdeadbeefdeadbeefdeadbeef':
