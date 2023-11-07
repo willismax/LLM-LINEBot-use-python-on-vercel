52cb87b2f0d3f258bf20264da01772768d12fcc3
willismax <willismax.com@gmail.com>
2023-11-07 21:51:42 +0800

    Upgrade to GPT 4

--- api/llm.py
+++ api/llm.py
@@ -10,14 +10,18 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4-1106-preview")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4-vision-preview")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 500))
 
     def get_response(self):
-        response = client.chat.completions.create(
+        # response = client.chat.completions.create(
+        response = client.completions.create(
             model=self.model,
-            messages=self.prompt.generate_prompt(),
+            # messages=self.prompt.generate_prompt(),
+            prompt=self.prompt.generate_prompt(),
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
         )
         return response.choices[0].message.content
 
