8656d3bfa64f2d922ae7cd9e4fdefaa5b1ffe907
willismax <willismax.com@gmail.com>
2024-04-23 21:27:43 +0800

    fix index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, json, abort
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -60,7 +60,7 @@ def handle_message(event):
         chatgpt.add_msg(f"AI:{reply_msg}\n")
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=f"助教:{reply_msg}"))
+            TextSendMessage(text=f"AI助教:{reply_msg}"))
     """
     This function handles LINE MessageAPI messages. 
     It checks if the message is a text message and processes it accordingly.
